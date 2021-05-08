7d04399af298a3999d3f3c75b9dd6e20509a7644
makshah <meetshah1997@gmail.com>
2021-05-08 15:42:40 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# E-Commerce-Project
+ E-Commerce Project crated using Node.js and React
---
+++ backend/data.js
@@ -0,0 +1,52 @@
+export default {
+    products:[
+        {
+            _id: '1',
+            name: 'Gaming Mouse 1',
+            category: 'Mouse',
+            image: '/images/m1.jpg',
+            brand: 'Logitech',
+            price: 60,
+            status: 'In Stock',
+            rating: 4.8,
+            numReviews: 100,
+            description: 'some description'
+        },
+        {
+            _id: '2',
+            name: 'Gaming Keyboard 1',
+            category: 'Keyboard',
+            image: '/images/k1.jpg',
+            brand: 'Logitech',
+            price: 70,
+            status: 'In Stock',
+            rating: 4.5,
+            numReviews: 102,
+            description: 'some description' 
+        },
+        {
+            _id: '3',
+            name: 'Gaming Mouse 2',
+            category: 'Mouse',
+            image: '/images/m2.jpg',
+            brand: 'DELL',
+            price: 80,
+            status: 'In Stock',
+            rating: 4.9,
+            numReviews: 75,
+            description: 'some description'
+        },
+        {
+            _id: '4',
+            name: 'Gaming Keyboard 2',
+            category: 'Keyboard',
+            image: '/images/k2.jpg',
+            brand: 'DELL',
+            status: 'In Stock',
+            price: 90,
+            rating: 4.9,
+            numReviews: 90,
+            description: 'some description' 
+        },
+]
+}
